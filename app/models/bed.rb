683b147bf7b23a1638a931a3523591d56bcebb81
Soni <kumarisoni957060@gmail.com>
2026-02-05 23:47:52 +0530

    Validations added for columns

--- app/models/bed.rb
+++ app/models/bed.rb
@@ -1,2 +1,23 @@
 class Bed < ApplicationRecord
+  # States
+  STATES = %w[available occupied maintenance].freeze
+  
+  # Validations
+  validates :bed_number, presence: true, uniqueness: true
+  validates :state, inclusion: { in: STATES }
+  validates :patient_name, presence: true, if: -> { state == 'occupied' }
+  validates :urgency_level, presence: true, if: -> { state == 'occupied' }
+  
+  # State checks
+  def available?
+    state == 'available'
+  end
+  
+  def occupied?
+    state == 'occupied'
+  end
+  
+  def maintenance?
+    state == 'maintenance'
+  end
 end
