75929ff0a9500808aeb05e999b85c29d93fd4b82
Soni <kumarisoni957060@gmail.com>
2026-02-25 16:28:23 +0530

    Add Netlify URL to CORS origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -17,7 +17,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     # Add your frontend domain after deployment
     origins "http://localhost:5173",
             "http://localhost:3000",
-            /https:\/\/.*\.vercel\.app$/  # Allow all Vercel domains
+            "https://rad-lollipop-ccda4a.netlify.app/",
+            /https:\/\/.*\.netlify\.app$/  # Allow all Vercel domains
 
     resource "*",
       headers: :any,
